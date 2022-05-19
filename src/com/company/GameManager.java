39cefe4387e12e6cd94d5c10822edc17d9b63c43
Kaitlyn Vitkin <you@example.com>
2022-05-19 12:20:09 -0400

    CSA 5/19

--- src/com/company/GameManager.java
+++ src/com/company/GameManager.java
@@ -15,8 +15,16 @@ public class GameManager {
         player2.addShips();
         player2.shipsToGrid(player2);
         while(player1.getShips().size() > 0 && player2.getShips().size() > 0){
-            
+            player1.makeTurn(player2);
+            player1.print
+            player2.makeTurn(player1);
+
         }
+        gameOver();
         input.close();
+    }
+
+    public void gameOver(){
+        m
     }
 }
